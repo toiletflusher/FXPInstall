83fdf3264808c76561394560e1c9007d4e2f65b9
Toiletflusher <80153347+toiletflusher@users.noreply.github.com>
2021-10-05 16:01:11 +0200

    separate config

---
+++ config.bat
@@ -0,0 +1,6 @@
+@echo off
+:: uncomment below to skip install, recovery, exit
+::set nomain=1
+:: uncomment options below to choose to automatically choose to partiton automatically or manually
+::set partauto=1
+::set partman=1
