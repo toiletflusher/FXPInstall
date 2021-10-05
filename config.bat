cce93bb255c25403146069e6c06c62a704a713e6
Toiletflusher <80153347+toiletflusher@users.noreply.github.com>
2021-10-05 20:09:24 +0200

    another new config option

--- config.bat
+++ config.bat
@@ -1,8 +1,9 @@
 @echo off
 :: uncomment below to skip install, recovery, exit
 ::set nomain=1
-:: uncomment options below to choose to automatically choose to partiton automatically or manually
+:: uncomment options below to choose to automatically choose to partiton automatically or manually, or uncomment and insert a number on diskn to automatically format disk {number}
 ::set partauto=1
 ::set partman=1
+::set diskn=0
 :: below is the cd name, change it to the iso OR cd name the cd/iso has
 set cdname=LantherNT
