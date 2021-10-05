196be37bf64219f886cd2dc849a471b002565687
Toiletflusher <80153347+toiletflusher@users.noreply.github.com>
2021-10-05 16:00:41 +0200

    separate config

--- LantherNT.cmd
+++ LantherNT.cmd
@@ -1,10 +1,5 @@
 @echo off
 color 18
-:: uncomment below to skip install, recovery, exit
-::set nomain=1
-:: uncomment options below to choose to automatically choose to partiton automatically or manually
-::set partauto=1
-::set partman=1
 cls
 title LantherNT
 echo.
